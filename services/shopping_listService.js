68a821c85a3bd5b4c5302427cfff74ed9295c50a
HAIDALA <91802818+HAIDALA@users.noreply.github.com>
2022-10-07 21:40:25 +0200

    Update shopping_listService.js

--- services/shopping_listService.js
+++ services/shopping_listService.js
@@ -1,60 +1,3 @@
-// // import { executeQuery } from "../database/database.js";
-
-// // const create = async (name) => {
-// //   await executeQuery("INSERT INTO shopping_lists (name) VALUES ($1);", name);
-// // };
-
-// // const desactive = async (name) => {
-// //   await executeQuery("UPDATE shopping_lists SET active = false WHERE name = '$1';",name);
-// // };
-
-
-// // const findAllActiveShopping_lists = async () => {
-// //   let result = await executeQuery(
-// //     "SELECT * FROM shopping_lists WHERE active  = true;",
-// //   );
-// //   return result.rows;
-// // };
-
-// // const findById = async (id) => {
-// //   let result = await executeQuery("SELECT * FROM shopping_lists  WHERE id = $1;", id);
-// //   if (result.rows && result.rows.length > 0) {
-// //     return result.rows[0];
-// //   }
-
-// //   return { id: 0, name: "Unknown" };
-// // };
-
-
-// // export { create, findAllActiveShopping_lists, findById, desactive };
-
-// import { executeQuery } from "../database/database.js";
-
-// const create = async (name) => {
-//   await executeQuery("INSERT INTO shopping_lists (name) VALUES ($1);", name);
-// };
-// const desactivateById = async (id) => {
-//   await executeQuery("UPDATE shopping_lists SET active = false WHERE id = $1;",id);
-// };
-// const findActiveShoppingList = async () => {
-//   let result = await executeQuery(
-//     "SELECT * FROM shopping_lists WHERE active = true;",
-//   );
-//   return result.rows;
-// };
-
-// const findById = async (id) => {
-//   let result = await executeQuery("SELECT * FROM shopping_lists  WHERE id = $1;", id);
-//   if (result.rows && result.rows.length > 0) {
-//     return result.rows[0];
-//   }
-
-//   return { id: 0, name: "Unknown" };
-// };
-
-
-// export { create, findActiveShoppingList, desactivateById, findById};
-
 import { executeQuery } from "../database/database.js";
 
 const create = async (name) => {
@@ -71,6 +14,8 @@ const findActiveShoppingList = async () => {
   return result.rows;
 };
 
+
+
 const findById = async (id) => {
   let result = await executeQuery("SELECT * FROM shopping_lists WHERE id = $id;", {
     id: id,
@@ -83,4 +28,30 @@ const findById = async (id) => {
   return { id: 0, name: "Unknown" };
 };
 
-export {create, findActiveShoppingList,  findById}
+const deactivate_shopping_list = async (id) => {
+  await executeQuery(
+    "UPDATE shopping_lists SET active = false WHERE id = $id;",
+    {
+      id: id,
+    }
+  );
+};
+
+
+
+const count_lists = async () => {
+  let result = await executeQuery(
+    "SELECT COUNT(id) FROM shopping_lists;"
+  );
+
+  return result.rows[0].count;
+};
+
+const count_items = async () => {
+  let result = await executeQuery(
+    "SELECT COUNT(id) FROM shopping_list_items;"
+  );
+
+  return result.rows[0].count;
+};
+export {create, findActiveShoppingList, findById, deactivate_shopping_list, count_lists, count_items}
