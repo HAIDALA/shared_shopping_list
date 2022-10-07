f5d0ee2d9ff490f4cfb24b8b4b35f04ad85f7722
HAIDALA <91802818+HAIDALA@users.noreply.github.com>
2022-10-07 20:13:01 +0200

    Update single_shopping_listController.js

--- controllers/single_shopping_listController.js
+++ controllers/single_shopping_listController.js
@@ -62,18 +62,32 @@ const addItem = async (request) => {
   const formData = await request.formData();
   const name = formData.get("name");
 
-  await single_shopping_listService.create_item(name);
+  await single_shopping_listService.create_item(name,urlParts[2]);
   
-  return requestUtils.redirectTo(`lists/${urlParts[2]}`);
+  return requestUtils.redirectTo(`/lists/${urlParts[2]}`);
 };
 
-const viewItems = async (request) => {
+const viewItems_signle_shopping_list = async (request) => {
+  const url = new URL(request.url);
+  const urlParts = url.pathname.split("/");
   const data = {
-    items: await single_shopping_listService.findAllNonCollectedItems(),
+    items: await single_shopping_listService.findAllNonCollectedItems(urlParts[2]),
+    collected_items : await single_shopping_listService.findAllCollectedItems(urlParts[2]),
+    current_signle_shopping_list: await single_shopping_listService.findCurrentShoppingList(urlParts[2]),
+
   };
 
   return new Response(await renderFile("single_shopping_list.eta", data), responseDetails);
 };
 
+const collectItem = async (request) => {
+  const url = new URL(request.url);
+  const urlParts = url.pathname.split("/");
+  await single_shopping_listService.collect_item(urlParts[4]);
 
-export { addItem, viewItems };
+  return requestUtils.redirectTo(`/lists/${urlParts[2]}`);
+};
+
+
+
+export { addItem, viewItems_signle_shopping_list, collectItem};
